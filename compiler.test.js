6fd3fc968aa9de47535b26cdda85388aa3cda085
Rudi Yardley <contact@rudiyardley.com>
2019-07-17 15:57:54 +0100

    Objects arrays are parsed

--- compiler.test.js
+++ compiler.test.js
@@ -123,30 +123,59 @@ it("tokens", () => {
 });
 
 [
-  { input: "_E0 || _E1", output: "_E0 _E1 ||" },
-  { input: "_E0 || _E1 && _E3", output: "_E0 _E1 _E3 && ||" },
-  { input: "!_E0 || _E1 && _E3", output: "_E0 ! _E1 _E3 && ||" },
-  { input: "!(_E0 || _E1) && _E3", output: "_E0 _E1 || ! _E3 &&" },
+  { input: "a||b", output: "a b ||" },
+  { input: "a||b&&c", output: "a b c && ||" },
+  { input: "!a||b&&c", output: "a ! b c && ||" },
+  { input: "!(a||b)&&c", output: "a b || ! c &&" },
   {
-    input: "[ _E0 , _E1 , _E2 , _E3 ] || _E4",
-    output: "_E0 _E1 _E2 _E3 [4 _E4 ||"
+    input: `
+  [
+    fee,
+    fi,
+    fo,
+    fum,
+    [
+      i,
+      smell,
+      the,
+      blood,
+      [
+        of,
+        an,
+        englishman
+      ]
+    ]
+  ]`,
+    output: "fee fi fo fum i smell the blood of an englishman [3 [5 [5"
   },
   {
-    input: "[ _E0, _E1, [ _E2 || _E3, _E4 ] ] && _E5",
-    output: "_E0 _E1 _E2 _E3 || _E4 [2 [3 _E5 &&"
+    input: "[a,b,c,d]||e",
+    output: "a b c d [4 e ||"
+  },
+  {
+    input: "[a,b,[c||d,e]]&&f",
+    output: "a b c d || e [2 [3 f &&"
   },
   {
     input: "{ name, age }",
     output: "name age {2"
   },
   {
-    input: "{ name : _E0, age : _E1 }",
-    output: "name _E0 : age _E1 : {2"
+    input: "{ name : a, age : b }",
+    output: "name a : age b : {2"
+  },
+  {
+    input: "{ name : [foo, bar], age : b }",
+    output: "name foo bar [2 : age b : {2"
+  },
+  {
+    input: "{ name : {foo:[1,2], bar}, age : b }",
+    output: "name foo 1 2 [2 : bar {2 : age b : {2"
   }
 ].forEach(({ input, output, skip, only }) => {
   const itFunc = skip ? it.skip : only ? it.only : it;
 
-  itFunc(input, () => {
+  itFunc(`"${input}" -> "${output}"`, () => {
     expect(parser(tokenizer(input)).join(" ")).toEqual(output);
   });
 });
