612e9564582943140c0109ef3cd702595af7ee02
Rudi Yardley <contact@rudiyardley.com>
2019-07-24 13:44:46 +0200

    Create typescript guard

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-export default function foo(
+export default function predicateCreator<T extends object>(
   strings: TemplateStringsArray,
   ...expressions: any[]
-): (a: any) => boolean;
+): (input: any) => input is T;
