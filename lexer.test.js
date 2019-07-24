f586cc01540d3cfdd33708aebf50785e4f4aac26
Rudi Yardley <contact@rudiyardley.com>
2019-07-24 12:17:41 +0200

    Comments (#26)

--- lexer.test.js
+++ lexer.test.js
@@ -1,7 +1,7 @@
 const { lexer } = require("./lexer");
 
 describe("lexer", () => {
-  it("tokens", () => {
+  it("should tokenize", () => {
     expect(
       lexer("   {   name   :   @{LINK:0} && @{LINK:2} ,age:@{LINK:1}}")
     ).toEqual([
@@ -17,5 +17,14 @@ describe("lexer", () => {
       "@{LINK:1}",
       "}"
     ]);
+  });
+  it("should strip comments", () => {
+    expect(
+      lexer(`{
+        name: "Rudi", // foo
+        // thing
+        age
+      }`)
+    ).toEqual(["{", "name", ":", '"Rudi"', ",", "age", "}"]);
   });
 });
