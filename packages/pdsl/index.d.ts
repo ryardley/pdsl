dc643a2d2c5a65ccb586ccefb7160b1a822ce490
Rudi Yardley <contact@rudiyardley.com>
2019-07-24 13:51:38 +0200

    Remove Typescript constraint

--- packages/pdsl/index.d.ts
+++ packages/pdsl/index.d.ts
@@ -1,4 +1,4 @@
-export default function predicateCreator<T extends object>(
+export default function predicateCreator<T>(
   strings: TemplateStringsArray,
   ...expressions: any[]
 ): (input: any) => input is T;
