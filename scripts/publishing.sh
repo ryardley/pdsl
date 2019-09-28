688c0e1fc1ee43edd04791435fb89342fee042b6
Rudi Yardley <contact@rudiyardley.com>
2019-09-28 15:16:56 +0300

    Consolodate publish scripts together

--- scripts/publishing.sh
+++ scripts/publishing.sh
@@ -3,3 +3,7 @@ publish_to_dist_tag() {
   local DIST_TAG = $1
   $(yarn bin)/lerna publish from-package --dist-tag "$DIST_TAG" --registry https://registry.npmjs.org/
 }
+
+promote_next_to_latest() {
+  $(yarn bin)/lerna exec --stream --no-bail --concurrency 1 -- 'PKG_VERSION=$(npm v . dist-tags.next); [ -n "$PKG_VERSION" ] && ( npm dist-tag add ${LERNA_PACKAGE_NAME}@${PKG_VERSION} latest ) || exit 0'
+}
