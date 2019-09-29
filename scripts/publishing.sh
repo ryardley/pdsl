7565991e3f23727cfebfa1c512e5c98015906646
Rudi Yardley <contact@rudiyardley.com>
2019-09-29 11:03:21 +0300

    Tidy scripts

--- scripts/publishing.sh
+++ scripts/publishing.sh
@@ -3,11 +3,13 @@ source ./scripts/standard-lib.sh
 publish_to_dist_tag() {
   local dist_tag = "$1"
   exit_if_empty "$dist_tag" "dist_tag is empty exiting."
-  exit_if_empty "$DANGEROUSLY_PUBLISH" "Mocking publishing to ${dist_tag}."
+  exit_if_empty "$LIVE_RUN" "Mocking publishing to ${dist_tag}."
   $(yarn bin)/lerna publish from-package --dist-tag "$dist_tag" --registry https://registry.npmjs.org/
 }
 
-promote_next_to_latest() {
-  exit_if_empty "$DANGEROUSLY_PUBLISH" "Mocking promoting next to latest."
-  $(yarn bin)/lerna exec --stream --no-bail --concurrency 1 -- 'pkg_version=$(npm v . dist-tags.next); [ -n "$pkg_version" ] && ( npm dist-tag add ${LERNA_PACKAGE_NAME}@${pkg_version} latest ) || exit 0'
+promote_tag_to_tag() {
+  local source_tag=${1:-next}
+  local destination_tag=${2:-latest}
+  exit_if_empty "$LIVE_RUN" "Mocking promoting next to latest."
+  $(yarn bin)/lerna exec --stream --no-bail --concurrency 1 -- "pkg_version=$(npm v . dist-tags.$source_tag); [ -n "$pkg_version" ] && ( npm dist-tag add ${LERNA_PACKAGE_NAME}@${pkg_version} $destination_tag ) || exit 0"
 }
