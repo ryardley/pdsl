69152f62d962b38208349aed1869fa13a7d50aa0
Rudi Yardley <contact@rudiyardley.com>
2019-12-12 19:44:32 +0200

    Update TypeScript types

--- packages/pdsl/index.d.ts
+++ packages/pdsl/index.d.ts
@@ -1,4 +1,30 @@
-export default function predicateCreator<T>(
+type PredicateFn<T> = (input: any) => input is T;
+
+type PdslOptions = {
+  schemaMode: boolean;
+  abortEarly: boolean;
+  captureErrors: boolean;
+  throwErrors: boolean;
+};
+
+type PdslError = {
+  path: string;
+  message: string;
+};
+
+type PdslSchema = {
+  validateSync(a: any): Array<PdslError>;
+  validate(a: any): Promise<Array<PdslError>>;
+};
+
+declare function predicateCreator<T>(
   strings: TemplateStringsArray,
   ...expressions: any[]
-): (input: any) => input is T;
+): PredicateFn<T>;
+
+declare namespace predicateCreator {
+  var schema: (options: PdslOptions) => PdslSchema;
+  var predicate: <T>(options: PdslOptions) => PredicateFn<T>;
+}
+
+export default predicateCreator;
