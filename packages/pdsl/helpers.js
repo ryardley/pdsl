d15657e59e9017978f8a6927f03ea5f647c08a07
Rudi Yardley <contact@rudiyardley.com>
2020-03-27 15:16:55 +1100

    Remove redundant code

--- packages/pdsl/helpers.js
+++ packages/pdsl/helpers.js
@@ -642,12 +642,7 @@ const createSchema = (compiler, ctx) => {
 
         // Throw errors
         if (ctx.throwErrors && errs.length > 0) {
-          if (errs.length > 0) {
-            throw new ValidationError("Validation failed", "", errs);
-          } else {
-            const [singleError] = errs;
-            throw new ValidationError(singleError.message, singleError.path);
-          }
+          throw new ValidationError("Validation failed", "", errs);
         }
 
         // Return the errors
