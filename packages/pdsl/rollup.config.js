e4a9d740f14436c5531917e9f0dcd4df081b6799
Rudi Yardley <contact@rudiyardley.com>
2020-04-03 15:18:45 +1100

    Fix up build and test process

--- packages/pdsl/rollup.config.js
+++ packages/pdsl/rollup.config.js
@@ -3,29 +3,51 @@ import typescript from "rollup-plugin-typescript2";
 const config = {
   watch: {
     include: "src/**"
-  },
-  plugins: [typescript({ rollupCommonJSResolveHack: false, clean: true })]
+  }
+};
+
+const tsconfig = {
+  rollupCommonJSResolveHack: false,
+  clean: true
 };
 
 export default [
   {
     input: "./src/helpers/index.ts",
     output: {
-      file: "./helpers.js",
+      file: "./helpers/index.js",
       name: "helpers",
       format: "cjs",
       exports: "named"
     },
+    plugins: [typescript(tsconfig)],
     ...config
   },
   {
     input: "./src/lib/index.ts",
     output: {
-      file: "./lib.js",
+      file: "./index.js",
       name: "lib",
       format: "cjs",
       exports: "named"
     },
+    plugins: [
+      typescript({
+        ...tsconfig,
+        tsconfig: "./tsconfig.declarations.json"
+      })
+    ],
+    ...config
+  },
+  {
+    input: "./src/lib/grammar.ts",
+    output: {
+      file: "./lib/grammar.js",
+      name: "lib",
+      format: "cjs",
+      exports: "named"
+    },
+    plugins: [typescript(tsconfig)],
     ...config
   }
 ];
